524016393712d2caf3fa37b88d0ce68de045cc1d
viccom <3520845@qq.com>
2018-10-15 18:14:59 +0800

    update

---
+++ delta_DVP_12SA2/userlib/linkcfg.lua
@@ -0,0 +1,11 @@
+local cfg = {
+    Link_type = "serial",
+    protocol = "rtu",
+    serial = { port = "/dev/ttymxc0", baudrate = 9600, data_bits = 8, parity = "NONE", stop_bits = 1, flow_control = "OFF" },
+    socket = { host = "192.168.199.132", port = 502, nodelay = true },
+    devs = {
+        {sn = "1", name = "delta_DVP_12SA2", addr="3", tpl="tpl1"},
+    }
+}
+
+return cfg
