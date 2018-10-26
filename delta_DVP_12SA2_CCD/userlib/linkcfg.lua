2bca76cc007af137bf217fdbe8a08f315c1d24b0
viccom <3520845@qq.com>
2018-10-26 16:20:04 +0800

    update

--- delta_DVP_12SA2_CCD/userlib/linkcfg.lua
+++ delta_DVP_12SA2_CCD/userlib/linkcfg.lua
@@ -1,7 +1,7 @@
 local cfg = {
     Link_type = "serial",
     protocol = "rtu",
-    serial = { port = "/dev/ttymxc0", baudrate = 9600, data_bits = 8, parity = "NONE", stop_bits = 1, flow_control = "OFF" },
+    serial = { port = "/dev/ttymxc0", baudrate = 115200, data_bits = 8, parity = "NONE", stop_bits = 1, flow_control = "OFF" },
     socket = { host = "192.168.199.132", port = 502, nodelay = true },
     devs = {
         {sn = "1", name = "tunliu_plc_DVP_12SA2_CCD", addr="3", tpl="tpl1"},
