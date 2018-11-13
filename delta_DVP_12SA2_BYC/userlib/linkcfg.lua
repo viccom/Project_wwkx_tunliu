d1cac99c817c14338486085eeb339d8330dadcd9
viccom <3520845@qq.com>
2018-11-13 18:42:12 +0800

    update default config

--- delta_DVP_12SA2_BYC/userlib/linkcfg.lua
+++ delta_DVP_12SA2_BYC/userlib/linkcfg.lua
@@ -4,7 +4,7 @@ local cfg = {
     serial = { port = "/dev/ttymxc1", baudrate = 115200, data_bits = 8, parity = "NONE", stop_bits = 1, flow_control = "OFF" },
     socket = { host = "192.168.199.132", port = 502, nodelay = true },
     devs = {
-        {sn = "1", name = "tunliu_plc_DVP_12SA2_BYC", addr="2", tpl="byc"},
+        {sn = "1", name = "tunliu_delta_byc", addr="2", tpl="byc"},
     }
 }
 
